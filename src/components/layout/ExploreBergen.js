91de89d44b0ae94154c855ba3cc7080937a120c1
Shamil Noroff <shamil_b93@hotmail.com>
2021-05-23 19:23:46 +0200

    Change

--- src/components/layout/ExploreBergen.js
+++ src/components/layout/ExploreBergen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Carousel from "react-bootstrap/carousel";
+import Carousel from "react-bootstrap/Carousel";
 import ExploreButton from "../buttons/ExploreButton";
 import { Link } from "react-router-dom";
 
